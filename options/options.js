fec1068d60465285155ed7ceb7102b04892f28ed
Crowdbotics <team@crowdbotics.com>
2021-08-16 20:47:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "virtual_robot_29858Identifier",
+  url: "https://virtual_robot_29858Identifier.botics.co",
+  api: "https://virtual_robot_29858Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
