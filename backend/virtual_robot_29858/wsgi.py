cbb77748ac81022243726508850e4ff952de72c5
Crowdbotics <team@crowdbotics.com>
2021-08-16 20:49:55 +0000

    Update models

--- backend/virtual_robot_29858/wsgi.py
+++ backend/virtual_robot_29858/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'virtual_robot_29858.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "virtual_robot_29858.settings")
 
 application = get_wsgi_application()
